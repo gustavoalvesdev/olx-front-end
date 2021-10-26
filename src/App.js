109d3bab1b35f0703e22f971ce84d2f09c1d90b3
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-10-26 10:50:42 -0300

    Criando o App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import './App.css';
 import Routes from "./Routes";
 
 import { Template } from "./components/MainComponents";
