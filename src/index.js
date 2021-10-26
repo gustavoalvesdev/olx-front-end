d2cb4b5f188211b25d2e09ca6b0abe2a0643521d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-10-25 23:57:02 -0300

    Configurando Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import Reducers from './Reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(Reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>    
+, document.getElementById('root'));
